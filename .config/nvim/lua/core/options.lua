3b379c2fb59900a0071f759efe2957b7bae1fee2
Elio Molinaro <emolinaro.work@gmail.com>
2025-12-01 15:26:04 +0100

    cleanup

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.localmapleader = " "
 
 vim.opt.autoindent = true
 vim.opt.expandtab = true
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,5 @@ return {
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
-    -- Optional dependencies
-    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }

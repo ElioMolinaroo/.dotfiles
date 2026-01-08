662e4e075020793260833f679c00bd637d607202
Elio Molinaro <emolinaro.work@gmail.com>
2026-01-08 14:27:57 +0100

    nvim: change ruff lineLength to 100

--- .config/nvim/lsp/ruff.lua
+++ .config/nvim/lsp/ruff.lua
@@ -3,5 +3,9 @@ return {
 	cmd = { "ruff", "server" },
 	filetypes = { "python" },
 	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
-	settings = {},
+	init_options = {
+		settings = {
+			lineLength = 100,
+		},
+	},
 }
